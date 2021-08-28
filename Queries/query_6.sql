3a2084979d2e27d7835879e5a3dfd02affa231b4
HaidarAliN <haidaralinehme@gmail.com>
2021-08-28 11:53:44 +0300

    optimizing query_6

--- Queries/query_6.sql
+++ Queries/query_6.sql
@@ -2,13 +2,12 @@ SELECT c.name as Name, count(fc.category_id) as Count  FROM film_category as fc
 join category as c using(category_id)
 group by fc.category_id
 having  IF(
-            (select count(b.a) from (SELECT count(fc.category_id) as a  FROM film_category as fc#test if there is counts between 55 and 65
-          group by fc.category_id
-          having  count(fc.category_id) BETWEEN 55 and 65) b) > 0
+            (select count(b.a) from (SELECT count(category_id) as a  FROM film_category#test if there is counts between 55 and 65
+          group by category_id
+          having  count(category_id) BETWEEN 55 and 65) b) > 0
     ,
           (COUNT(fc.category_id)>55 AND COUNT(fc.category_id)<65) #if yes return them
     ,
-          COUNT(fc.category_id) = (select max(b.a) from (SELECT count(fc.category_id) as a  FROM film_category as fc#if no return max category count
-          group by fc.category_id
-          having  max(fc.category_id)) b))
+          COUNT(fc.category_id) = (select max(b.a) from (SELECT count(category_id) as a  FROM film_category#if no return max category count
+          group by (category_id)) b))
 order by count(fc.category_id) desc;
